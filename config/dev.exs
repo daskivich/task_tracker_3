1fa0c1819b553432118065f37a098b944a76754b
Dan Daskivich <daskivich.d@husky.neu.edu>
2018-03-28 08:19:41 -0400

    Configured Phoenix project (brunch, jquery, bootstrap, underscore, sass) and created database.

--- config/dev.exs
+++ config/dev.exs
@@ -51,8 +51,8 @@ config :phoenix, :stacktrace_depth, 20
 # Configure your database
 config :task_tracker_3, TaskTracker3.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
+  username: "task_tracker_3",
+  password: "Ushoo4fuwuhe",
   database: "task_tracker_3_dev",
   hostname: "localhost",
   pool_size: 10
